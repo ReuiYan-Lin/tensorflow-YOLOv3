b8cb7b7569f5cbecf9f928095624dc07b56ed395
ReuiYan <40434344+ReuiYan-Lin@users.noreply.github.com>
2019-05-06 19:32:19 +0800

    Add files via upload

--- utils/tools.py
+++ utils/tools.py
@@ -38,6 +38,24 @@ def iou_calc(boxes1, boxes2):
     IOU = 1.0 * inter_area / union_area
     return IOU
 
+def iou_calc1(boxes1, boxes2):
+
+    boxes1 = np.array(boxes1)
+    boxes2 = np.array(boxes2)
+
+    boxes1_area = (boxes1[..., 2] - boxes1[..., 0]) * (boxes1[..., 3] - boxes1[..., 1])
+    boxes2_area = (boxes2[..., 2] - boxes2[..., 0]) * (boxes2[..., 3] - boxes2[..., 1])
+
+
+    left_up = np.maximum(boxes1[..., :2], boxes2[..., :2])
+    right_down = np.minimum(boxes1[..., 2:], boxes2[..., 2:])
+
+
+    inter_section = np.maximum(right_down - left_up, 0.0)
+    inter_area = inter_section[..., 0] * inter_section[..., 1]
+    union_area = boxes1_area + boxes2_area - inter_area
+    IOU = 1.0 * inter_area / union_area
+    return IOU
 
 def GIOU(boxes1, boxes2):
 
